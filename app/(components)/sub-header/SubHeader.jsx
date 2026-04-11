7e6f6eee7a15b4232562a2882727f70a2b50ef7e
Abdelrhman-lang <abdokhaled766@gmail.com>
2026-04-11 18:18:34 +0200

    fix header issues update 1.2

--- app/(components)/sub-header/SubHeader.jsx
+++ app/(components)/sub-header/SubHeader.jsx
@@ -6,7 +6,7 @@ import { SelectDemo } from "../select-demo/SelectDemo";
 export default function SubHeader() {
     const pathname = usePathname()
     return (
-        <section className={`py-3 px-8 bg-primary  ${pathname.includes("sign-in") || pathname.includes("sign-up") ? "hidden" : "block"}`}>
+        <section className={`py-3 px-8 bg-primary ${pathname.includes("sign-in") || pathname.includes("sign-up") ? "hidden" : "hidden lg:flex"}`}>
             <div className='container mx-auto'>
                 <div className='flex items-center justify-between'>
                     <div className='px-3 flex items-center gap-4'>
